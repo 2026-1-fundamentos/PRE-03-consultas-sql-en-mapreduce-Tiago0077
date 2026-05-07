4a5d9e1710d905f301afbadc04c8ee576ba61199
Tiago0077 <sazuluagal@unal.edu.co>
2026-05-06 21:59:49 -0500

    Update mapreduce.py

--- homework/mapreduce.py
+++ homework/mapreduce.py
@@ -1,106 +1,37 @@
 import glob
 import os
 
-import pandas as pd  # type: ignore
+def hadoop(input_folder, output_folder, mapper_fn, reducer_fn):
 
-
-def load_input(input_directory):
-    """Load text files in 'input_directory/'"""
-    #
-    # Lea los archivos de texto en la carpeta input/ y almacene el contenido en
-    # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
-    # entrada en el DataFrame.
-    #
-    files = glob.glob(f"{input_directory}/*")
-    dataframes = [
-        pd.read_csv(
-            file,
-            header=None,
-            delimiter="\t",
-            names=["line"],
-            index_col=None,
-        )
-        for file in files
-    ]
-
-    dataframe = pd.concat(dataframes, ignore_index=True)
-
-    return dataframe
-
-
-def clean_text(dataframe):
-    """Text cleaning"""
-    #
-    # Elimine la puntuación y convierta el texto a minúsculas.
-    #
-    dataframe = dataframe.copy()
-    dataframe["line"] = dataframe["line"].str.lower()
-    dataframe["line"] = (
-        dataframe["line"]
-        .str.replace(",", "")
-        .str.replace(
-            ".",
-            "",
-        )
-    )
-    return dataframe
-
-
-def count_words(dataframe):
-    """Word count"""
-
-    dataframe = dataframe.copy()
-    dataframe["line"] = dataframe["line"].str.split()
-    dataframe = dataframe.explode("line")
-    dataframe = dataframe.groupby("line").size().reset_index(name="count")
-    return dataframe
-
-
-def save_output(dataframe, output_directory):
-    """Save output to a file."""
-
-    if os.path.exists(output_directory):
-        files = glob.glob(f"{output_directory}/*")
+    def read_records_from_input(input_folder):
+        sequence = []
+        files = glob.glob(f"{input_folder}*")
         for file in files:
-            os.remove(file)
-        os.rmdir(output_directory)
+            with open(file, "r", encoding="utf-8") as f:
+                for line in f:
+                    sequence.append((file, line))
+        return sequence
 
-    os.makedirs(output_directory)
+    def save_results_to_output(result):
+        with open(f"{output_folder}part-00000", "w", encoding="utf-8") as f:
+            for key, value in result:
+                f.write(f"{key}\t{value}\n")
 
-    dataframe.to_csv(
-        f"{output_directory}/part-00000",
-        sep="\t",
-        index=False,
-        header=False,
-    )
+    def create_success_file(output_folder):
+        with open(os.path.join(output_folder, "_SUCCESS"), "w", encoding="utf-8") as f:
+            f.write("")
 
+    def create_output_directory(output_folder):
+        if os.path.exists(output_folder):
+            raise FileExistsError(f"The folder '{output_folder}' already exists.")
+        else:
+            os.makedirs(output_folder)
 
-#
-# La siguiente función crea un archivo llamado _SUCCESS en el directorio
-# entregado como parámetro.
-#
-def create_marker(output_directory):
-    """Create Marker"""
-
-    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
-        f.write("")
-
-
-#
-# Escriba la función job, la cual orquesta las funciones anteriores.
-#
-def run_job(input_directory, output_directory):
-    """Job"""
-    dataframe = load_input(input_directory)
-    dataframe = clean_text(dataframe)
-    dataframe = count_words(dataframe)
-    save_output(dataframe, output_directory)
-    create_marker(output_directory)
-
-
-if _name_ == "_main_":
-
-    run_job(
-        "files/input",
-        "files/output",
-    )
+    sequence = read_records_from_input(input_folder)
+    pairs_sequence = mapper_fn(sequence)
+    pairs_sequence = sorted(pairs_sequence)
+    result = reducer_fn(pairs_sequence)
+    create_output_directory(output_folder)
+    save_results_to_output(result)
+    create_success_file(output_folder)
+  
